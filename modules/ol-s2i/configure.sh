55ecf1e90ed407d8cce34398384c186ce5f2b285
Brent Daniel <brenthdaniel@gmail.com>
2019-10-01 07:07:43 -0700

    Clean up assemble script

--- modules/ol-s2i/configure.sh
+++ modules/ol-s2i/configure.sh
@@ -4,8 +4,9 @@ set -e
 
 SCRIPT_DIR=$(dirname $0)
 ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
+WLP_DIR=/opt/ol
 
-chown -R 1001 /opt/ol
+chown -R 1001 $WLP_DIR
 chown -R 1001 $SCRIPT_DIR
 chmod -R ug+rwX $SCRIPT_DIR
 chmod ug+x ${ARTIFACTS_DIR}/usr/local/s2i/*
