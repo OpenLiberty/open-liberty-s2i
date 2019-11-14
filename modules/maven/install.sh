a940cfb16739483489abb2cf51c703065e1e8f3f
Brent Daniel <brenthdaniel@gmail.com>
2019-11-14 13:31:05 -0800

    Add new test

--- modules/maven/install.sh
+++ modules/maven/install.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-microdnf install gzip
-
     tar -C /usr/local -zxf /tmp/artifacts/maven.tar.gz \
       && ln -sf /usr/local/apache-maven-3.5.4/bin/mvn /usr/local/bin/mvn 
 
-microdnf remove gzip
+SCRIPT_DIR=$(dirname $0)
+ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
+pushd ${ARTIFACTS_DIR}
 
 mkdir  -p /home/default/.m2/repository
 chown -R 1001 /home/default/.m2
 chmod -R ug+rwX /home/default/.m2
+
+ls
+cp settings.xml /home/default/.m2
+popd
+
