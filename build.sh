548336c9bb07d28b6b4d2177718ec7fcca3314fc
Kevin Ortega <ortegake@us.ibm.com>
2026-07-16 08:48:30 -0400

    Enable Docker BuildKit in build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR=$(dirname $0)
 export DOCKER_API_VERSION="1.41"
 export DOCKER_TIMEOUT="1200"
+export DOCKER_BUILDKIT=0
 
 JAVA8_BASE_IMAGE_NAME="${NAMESPACE:=icr.io}/appcafe/open-liberty:${LIBERTY_VERSION}-full-java8-openj9-ubi"
 JAVA11_BASE_IMAGE_NAME="${NAMESPACE:=icr.io}/appcafe/open-liberty:${LIBERTY_VERSION}-full-java11-openj9-ubi"
